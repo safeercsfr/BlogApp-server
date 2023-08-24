ef0f6db81b4e5a6e8664eacecdabe95d4babe448
safeercsfr <safeermohd0009@gmail.com>
2023-08-24 13:07:40 +0530

    add changes

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const authRoutes = require("./routes/authRoutes.js");
 const postRoutes = require("./routes/postRoutes.js");
 
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
@@ -49,6 +50,7 @@ app.use("/api/posts", postRoutes);
 app.use("/", (req, res) => {
   res.send("works");
 });
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
